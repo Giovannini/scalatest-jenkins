9735761b4b639dd03a6d12580c9df3e0bf5f0718
Kevin LAURENT <kevin.laurent@fabernovel.com>
2017-12-01 10:42:08 +0100

    comment on issue

--- src/scalaTestPlugin/src/main/scala/io.giovannini/Main.scala
+++ src/scalaTestPlugin/src/main/scala/io.giovannini/Main.scala
@@ -1,19 +1,22 @@
 package io.giovannini
 
 import java.io.{File, FileWriter}
-import scala.io.Source
 
 import com.softwaremill.sttp._
 import io.giovannini.model.PullRequestMessage
 
+import scala.io.Source
+
 object Main {
   implicit val backend: SttpBackend[Id, Nothing] = HttpURLConnectionBackend()
-  val modifiedFiles: Seq[String] = getModifiedFiles
+  val modifiedFiles: Seq[String] = getModifiedFiles("modifiedFiles")
+  val allfiles: Seq[String] = getModifiedFiles("allfiles")
 
   def main(args: Array[String]): Unit = {
     args.foreach(println)
     val testReportsDirectoryName = args(0)
     val ghprbPullLink = args(1)
+    val ghprbIssueLink: String = args(3)
     val ghprbActualCommit = args(2)
 
 
@@ -28,39 +31,13 @@ object Main {
         testCase <- scala.xml.XML.loadFile(file) \\ "testsuite" \\ "testcase"
         failure <- testCase \\ "failure"
         message <- failure.attribute("message").toSeq
-        
-      } yield PullRequestMessage(testCase, message, modifiedFiles)
+      } yield PullRequestMessage(testCase, message, modifiedFiles, allfiles)
 
       pullRequestMessages
-        .foreach(p => sendMessageOnGitHub(ghprbPullLink, ghprbActualCommit, p))
+        .foreach(p => p.send(ghprbActualCommit, ghprbPullLink, ghprbIssueLink))
 
       writeToFile(s"""{"messages": [${pullRequestMessages.map(PullRequestMessage.writes).mkString(", ")}]}""")
     }
-  }
-
-  def sendMessageOnGitHub(
-    ghprbPullLink: String,
-    ghprbActualCommit: String,
-    pullRequestMessage: PullRequestMessage
-  ): Unit = {
-    val body = s"""
-                  |{
-                  |"body": "${pullRequestMessage.message}",
-                  |"commit_id": "$ghprbActualCommit",
-                  |"path": "${pullRequestMessage.fileName}",
-                  |"position": ${pullRequestMessage.line.getOrElse(1)}
-                  |}
-        """.stripMargin
-    val request = sttp.post(uri"$ghprbPullLink")
-        .header("Authorization", s"Bearer " + "3c114163b1" + "659f0d8f607838" +
-          "bb193c122a30dc48")
-        .header("Content-Type", "application/json")
-      .body(body)
-
-    println("Request body: " + body)
-    val response = request.send()
-    println("Response code: " + response.code)
-    println("Response body: " + response.body)
   }
 
   private def writeToFile(s: String): Unit = {
@@ -71,6 +48,6 @@ object Main {
     finally fw.close()
   }
 
-  private def getModifiedFiles: Seq[String] =
-    Source.fromFile("modifiedFiles").getLines().toSeq
+  private def getModifiedFiles(source:String): Seq[String] =
+    Source.fromFile(source).getLines().toSeq
 }
